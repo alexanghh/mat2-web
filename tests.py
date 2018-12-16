a71a39014519b1c1538fa173bb17f1de814246bf
jvoisin <julien.voisin@dustri.org>
2018-12-16 21:33:18 +0100

    Add even more tests

--- tests.py
+++ tests.py
@@ -1,4 +1,7 @@
 import unittest
+import tempfile
+import shutil
+import io
 
 import main
 
@@ -6,7 +9,11 @@ import main
 class FlaskrTestCase(unittest.TestCase):
     def setUp(self):
         main.app.testing = True
+        main.app.config['UPLOAD_FOLDER'] = tempfile.mkdtemp()
         self.app = main.app.test_client()
+
+    def tearDown(self):
+        shutil.rmtree(main.app.config['UPLOAD_FOLDER'])
 
     def test_get_root(self):
         rv = self.app.get('/')
@@ -19,6 +26,27 @@ class FlaskrTestCase(unittest.TestCase):
     def test_get_download_nonexistant_file(self):
         rv = self.app.get('/download/non_existant')
         self.assertEqual(rv.status_code, 302)
+
+    def test_get_upload_without_file(self):
+        rv = self.app.post('/')
+        self.assertEqual(rv.status_code, 302)
+
+    def test_get_upload_empty_file(self):
+        rv = self.app.post('/',
+                data=dict(
+                    file=(io.BytesIO(b""), 'test.pdf'),
+                    ), follow_redirects=False)
+        self.assertEqual(rv.status_code, 302)
+
+    def test_get_upload_empty_file_redir(self):
+        rv = self.app.post('/',
+                data=dict(
+                    file=(io.BytesIO(b""), 'test.pdf'),
+                    ), follow_redirects=True)
+        self.assertIn(b'The type application/pdf is not supported',
+                rv.data)
+        self.assertEqual(rv.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()

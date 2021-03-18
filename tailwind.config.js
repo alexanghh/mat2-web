7f253f71866a35a4c681a9ac37109d0ea4ab3d0e
Jfriedli <jan.friedli@immerda.ch>
2021-03-18 10:38:18 +0100

    started restyling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,24 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     future: {
-        // removeDeprecatedGapUtilities: true,
-        // purgeLayersByDefault: true,
     },
     purge: [],
     theme: {
         extend: {
+            colors: {
+                blue: {
+                    light: '#f4f7fb',
+                    DEFAULT: '#99c1f1',
+                    dark: '#1b5eb4',
+                }
+            },
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
         },
     },
     variants: {},
-    plugins: [],
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
